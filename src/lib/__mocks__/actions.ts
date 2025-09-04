89db4642c8983def44d71d5b434ef6caec9d7682
Cliff Kabele Toge (Weirdo_Cliff) <kabelecliff@gmail.com>
2025-09-04 12:09:54 +0000

    remove all project test files

--- src/lib/__mocks__/actions.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-export const convertCurrency = jest.fn(async (amount: number, from: string, to: string) => {
-  if (from === 'USD' && to === 'NGN') {
-    return { success: true, convertedAmount: amount * 1500 };
-  }
-  return { success: true, convertedAmount: amount * 0.00067 };
-});
